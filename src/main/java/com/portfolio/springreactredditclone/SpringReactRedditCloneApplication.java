5882d650d421bfc4e6809ed62f1b48baa5373153
Christian Soto <christiansotogw2@gmail.com>
2021-05-25 14:23:40 -0400

    Implement Spring Security for APIs.

--- src/main/java/com/portfolio/springreactredditclone/SpringReactRedditCloneApplication.java
+++ src/main/java/com/portfolio/springreactredditclone/SpringReactRedditCloneApplication.java
@@ -1,5 +1,6 @@
 package com.portfolio.springreactredditclone;
 
+import com.portfolio.springreactredditclone.model.VerificationToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class SpringReactRedditCloneApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringReactRedditCloneApplication.class, args);
     }
-
 }
