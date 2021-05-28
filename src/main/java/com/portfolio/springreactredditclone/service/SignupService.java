5148ca7ac7e386f754f88ca6eda4acc38447250b
Christian Soto <christiansotogw2@gmail.com>
2021-05-28 16:15:52 -0400

    Implement email verification

--- src/main/java/com/portfolio/springreactredditclone/service/SignupService.java
+++ src/main/java/com/portfolio/springreactredditclone/service/SignupService.java
@@ -2,7 +2,10 @@ package com.portfolio.springreactredditclone.service;
 
 import com.portfolio.springreactredditclone.dto.RegisterRequest;
 import com.portfolio.springreactredditclone.model.User;
+import com.portfolio.springreactredditclone.model.VerificationToken;
 import com.portfolio.springreactredditclone.repository.UserRepository;
+import com.portfolio.springreactredditclone.repository.VerificationTokenRepository;
+import com.portfolio.springreactredditclone.utli.Constants;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
@@ -10,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
+import static com.portfolio.springreactredditclone.utli.Constants.ACTIVATION_EMAIL;
 import static java.time.Instant.now;
 
 @Service
@@ -19,10 +25,24 @@ public class SignupService {
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private VerificationTokenRepository verificationTokenRepository;
+    private MailContentBuilder mailContentBuilder;
 
     @Transactional
     public void signup(RegisterRequest registerRequest){
-        userRepository.save(createUser(registerRequest));
+        var user = createUser(registerRequest);
+        userRepository.save(user);
+        var token = generateVerificationToken(user);
+        mailContentBuilder.build(emailMessage(token));
+    }
+
+    private String generateVerificationToken(User user){
+        var token = UUID.randomUUID().toString();
+        var verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 
     @NotNull
@@ -38,5 +58,14 @@ public class SignupService {
 
     private String encodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    private String emailMessage(String token){
+        var stringBuilder = new StringBuilder();
+        var message = stringBuilder.append("Thank you for registering to the Spring Reddit Clone, please click on the url to activate your account : ")
+                .append(ACTIVATION_EMAIL)
+                .append("/")
+                .append(token).toString();
+        return message;
     }
 }
